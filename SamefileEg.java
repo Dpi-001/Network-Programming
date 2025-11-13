74ab3f31dde4ac70559924cff3f471e75b43c0f1
Dpi-001 <sudip9845197918@gmail.com>
2025-11-13 13:54:44 +0545

    SameFile Method Check

---
+++ SamefileEg.java
@@ -0,0 +1,16 @@
+import java.net.*;
+public class SamefileEg {
+public static void main (String[] args) {
+try {
+URL u1 = new URL("http://www.ncsa.uiuc.edu/HTMLPrimer.html#GS");
+URL u2 = new URL("http://www.ncsa.uiuc.edu/HTMLPrimer.html#HD");
+if (u1.sameFile(u2)) {
+System.out.println(u1 + " is the same file as \n" + u2);
+} else {
+System.out.println(u1 + " is not the same file as \n" + u2);
+}
+} catch (MalformedURLException ex) {
+System.err.println(ex);
+}
+}
+}
